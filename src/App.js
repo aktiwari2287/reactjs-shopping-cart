ca9fa12ce8d5796aaf3b5961dfe906dc8d02c598
Tiwari <AnandKumar.Tiwari@gartner.com>
2021-06-25 19:28:11 +0530

    added products from json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import React from "react";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+import Products from "./components/Products";
+import data from './data.json';
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        products: data.products,
+        size: "",
+        sort: ""
+      }
+    }
+    render() {
+      return (
+        <div className="grid-container">
+        <header>
+            <a href="/"> My Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+                <Products products={this.state.products}/>
+            </div>
+            <div className="sidebar">
+              Cart Items
+            </div>
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+      );
+    }
 }
 
 export default App;
